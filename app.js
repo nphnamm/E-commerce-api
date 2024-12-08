baffb55a8f9b18126a090dce5dc929eef2a5e4cd
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-12-08 16:16:54 +0700

    update for host

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://example.com", "http://e-commerce-chi-olive-70.vercel.app"],
+    origin: [
+      "http://localhost:3000", 
+      "https://e-commerce-chi-olive-70.vercel.app",
+      "https://e-commerce-git-main-nguyn-phan-hoai-nams-projects.vercel.app/",
+      "https://e-commerce-l51377asc-nguyn-phan-hoai-nams-projects.vercel.app/"
+    
+    ],
     credentials: true,
   })
 );
